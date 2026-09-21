fbee040de3dea9fabb101857e9bf129a565da59f
Fabri <azfabri4@gmail.com>
2026-09-21 19:34:02 -0300

    Corrigiendo el bug del punto para que solo se pueda poner una vez por termino,factores o comoponentes, tambien que no se pueda poner despues de cualquier operador, tambien corrigiendoel boton del menos que se superponia por el CE.

--- tronco.py
+++ tronco.py
@@ -12,7 +12,6 @@ class Interfaz(tk.Tk):
         self.auxiliar = ""
         self.entrada_de_calculadora()
         self.botones_numerales()
-        self.botones_especiales()
         self.mainloop()
     
     def mostrar_numeros(self,chars):
@@ -26,8 +25,23 @@ class Interfaz(tk.Tk):
             except IndexError:
                 pass
         elif chars == ".":
-            if "." in self.auxiliar:
+            if "." in self.auxiliar[-1]:
                 pass
+            elif "." in self.auxiliar:
+                indices = []
+                for e in ["+","-","*","/"]:
+                    indice = int(self.auxiliar.rfind(e))
+                    indices.append(indice)
+                maximo = max(indices)
+                texto_max = self.auxiliar[maximo:-1]
+                texto_min = self.auxiliar[:]
+                if "." not in texto_min:
+                    self.auxiliar += chars
+                    self.entrada_var.set(self.auxiliar)
+                    print("primero")
+                elif "." not in texto_max and texto_max:
+                    self.auxiliar += chars
+                    self.entrada_var.set(self.auxiliar)
             else:
                 self.auxiliar += chars
                 self.entrada_var.set(self.auxiliar)
@@ -42,6 +56,7 @@ class Interfaz(tk.Tk):
             self.auxiliar = ""
         except SyntaxError:
             self.entrada_var.set("")
+            self.auxiliar = ""
         except ZeroDivisionError:
             self.entrada_var.set("Numero indefinido")
             self.auxiliar = ""
@@ -53,53 +68,44 @@ class Interfaz(tk.Tk):
 
     
     def entrada_de_calculadora(self):
-        entrada = tk.Entry(self,textvariable=self.entrada_var,bg="sienna2",relief="flat",bd=0)
-        entrada.grid(column=0,row=0,pady=5,padx=3,rowspan=3,columnspan=3,sticky="wesn")
+        frame_entrada = tk.Frame(self)
+        frame_entrada.grid(row=0,column=0,sticky="nsew")
+        entrada = tk.Entry(frame_entrada,textvariable=self.entrada_var,bg="sienna2",relief="flat",bd=0)
+        entrada.grid(column=0,row=0,sticky="wesn")
         
     
     def botones_numerales(self,event=None):
+        
         frame_botones = tk.Frame(self,bg="gray26")
-        frame_botones.grid(row=4,column=0,sticky="we")
+        frame_botones.grid(row=4,column=0,sticky="we",padx=2,pady=2)
         botones = [("7",0,0),("8",0,1),("9",0,2),
                    ("4",1,0),("5",1,1),("6",1,2),
-                   ("1",2,0),("2",2,1),("3",2,2)]
+                   ("1",2,0),("2",2,1),("3",2,2),
+                   (".",3,0),("0",3,1),("=",3,2)]
         for t,r,c in botones:
-            botono_de_la_calculadora = tk.Button(frame_botones,text=t,bg="DarkOrange3",
-                                                 relief="flat",bd=0,font=("Arial",15),
-                                                 fg="CadetBlue1",command=lambda e=t:self.mostrar_numeros(e))
-            botono_de_la_calculadora.grid(row=r,column=c,padx=10,pady=5)
-    
-    def botones_especiales(self,event=None):
-        frame_botones_especiales_laterales = tk.Frame(self,bg="gray26")
-        frame_botones_especiales_laterales.grid(row=4,column=99,sticky="we",padx=5,pady=5)
-        frame_botones_especiales_inferiores = tk.Frame(self,bg="gray26")
-        frame_botones_especiales_inferiores.grid(row=5,column=0,sticky="ew")
-        botones_especiales = [("+",0,0),("-",1,0),("*",2,0),("/",0,1),("CE",1,1)]
-        botones_inferiores = [(".",0,0),("0",0,1),("=",0,2)]
+            if t == "=":
+                boton_de_la_calculadora = tk.Button(frame_botones,text=t,
+                                                    bg="DarkOrange4",
+                                                    relief="flat",bd=0,font=("Arial",15),
+                                                    fg="CadetBlue1",command=lambda :self.resultado())
+            else:
+                boton_de_la_calculadora = tk.Button(frame_botones,text=t,bg="DarkOrange3",
+                                                    relief="flat",bd=0,font=("Arial",15),
+                                                    fg="CadetBlue1",command=lambda e=t:self.mostrar_numeros(e))
+            boton_de_la_calculadora.grid(row=r,column=c,padx=10,pady=5)
+        botones_especiales = [("+",0,3),("-",1,3),("*",2,3),("/",0,4),("CE",3,3)]
         for t,r,c in botones_especiales:
             if t == "CE":
-                botones_especiales_calcu = tk.Button(frame_botones_especiales_laterales,text=t,
+                botones_especiales_calcu = tk.Button(frame_botones,text=t,
                                                  bg="DarkOrange4",
                                                  relief="flat",bd=0,font=("Arial",15),
                                                  fg="CadetBlue1",command=self.borrar)
             else:
-                botones_especiales_calcu = tk.Button(frame_botones_especiales_laterales,text=t,
+                botones_especiales_calcu = tk.Button(frame_botones,text=t,
                                                                  bg="DarkOrange4",
                                                                  relief="flat",bd=0,font=("Arial",15),
                                                                  fg="CadetBlue1",command=lambda e=t:self.mostrar_numeros(e))
             botones_especiales_calcu.grid(row=r,column=c,padx=5,pady=5)
-        for t,r,c in botones_inferiores:
-            if t == "=":
-                botones_inferiores_calcu = tk.Button(frame_botones_especiales_inferiores,text=t,
-                                                 bg="DarkOrange4",
-                                                 relief="flat",bd=0,font=("Arial",15),
-                                                 fg="CadetBlue1",command=lambda :self.resultado())
-            else:
-                botones_inferiores_calcu = tk.Button(frame_botones_especiales_inferiores,text=t,
-                                                                 bg="DarkOrange4",
-                                                                 relief="flat",bd=0,font=("Arial",15),
-                                                                 fg="CadetBlue1",command=lambda e=t:self.mostrar_numeros(e))
-            botones_inferiores_calcu.grid(row=r,column=c,padx=10,pady=5)
     
     
 
